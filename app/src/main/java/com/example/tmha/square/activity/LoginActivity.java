1f858a35a6a69b564fcbf0930370303b1203fb20
akashjpro <akashjpro@gmail.com>
2017-07-04 06:49:56 +0700

    commit

--- app/src/main/java/com/example/tmha/square/activity/LoginActivity.java
+++ app/src/main/java/com/example/tmha/square/activity/LoginActivity.java
@@ -4,16 +4,32 @@ import android.content.Intent;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tmha.square.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 
 
 /*
@@ -30,11 +46,24 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "TAG";
+    private final int RC_SIGN_IN = 123;
     private Button btnLogin, btnGoogle, btnFacebook;
     private ImageView mImgIcon;
     private TextView mTxtTitle;
     private DatabaseReference mDatabase;
     private LinearLayout mLayout;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private GoogleApiClient mGoogleApiClient;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +75,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void addControls() {
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
         btnLogin    = (Button) findViewById(R.id.buttonLogin);
         btnGoogle   = (Button) findViewById(R.id.buttonGoogle);
         btnFacebook = (Button) findViewById(R.id.buttonFacebook);
@@ -76,6 +108,28 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.child("Hello").setValue("KKKKKKKKKKKKKKKK");
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(LoginActivity.this)
+//                        .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                if (user != null){
+//                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+                }
+            }
+        };
     }
 
     private void addEvents() {
@@ -92,15 +146,69 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     login();
                 break;
             case R.id.buttonGoogle:
+                loginWithGoogle();
                 break;
             case R.id.buttonFacebook:
                 break;
         }
     }
 
+    private void loginWithGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
     private void login(){
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.left_in, R.anim.right_out);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+//                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+                Toast.makeText(this, "Fail authenticate with Google", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/tmha/square/fragment/SettingFragment.java
+++ app/src/main/java/com/example/tmha/square/fragment/SettingFragment.java
@@ -8,12 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tmha.square.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by tmha on 6/8/2017.
@@ -22,29 +19,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class SettingFragment extends Fragment implements OnMapReadyCallback{
     MapFragment mapFragment;
     GoogleMap mMap;
+    private View mView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        mView = inflater.inflate(R.layout.fragment_setting, container, false);
         addControls();
-        return view;
+        return mView;
     }
 
     private void addControls() {
-        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragMap);
-        mapFragment.getMapAsync(this);
+//        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragMap);
+//        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        // Add a marker in Sydney and move the camera
-        LatLng toi = new LatLng(10.862357, 106.619517);
-        mMap.addMarker(new MarkerOptions().position(toi)
-                .title("Cho tao dang o ne may")
-                .snippet("To hop anh hung vo lam"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toi, 16));
+//        mMap = googleMap;
+//        // Add a marker in Sydney and move the camera
+//        LatLng toi = new LatLng(10.862357, 106.619517);
+//        mMap.addMarker(new MarkerOptions().position(toi)
+//                .title("Cho tao dang o ne may")
+//                .snippet("To hop anh hung vo lam"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(toi, 16));
     }
 }
